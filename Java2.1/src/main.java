6ca394cded2187dc51ddaedf6e7ed72d28621c53
Keirahn <d.v.kudryashev@gmail.com>
2021-04-23 19:28:15 +0300

    project added

---
+++ Java2.1/src/main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args) {
+        int ticket = 1200; // ввести стоимость билета;
+        long miles = ticket / 20;
+        System.out.println(miles);
+    }
+}
